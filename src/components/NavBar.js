796e33cd38a8df471cfdcca4a0c6c02d01354000
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-17 17:49:33 -0500

    Added click handlers

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,9 +5,9 @@ export default function NavBar({ currentPage, handlePageChange }) {
     <div>
       <ul className='nav nav-tabs'>
         <li className='nav-item'><h1 href='#about' onClick={() => handlePageChange('About')}>Andrew Yoo</h1></li>
-        <li className='nav-item'><a href='#home'>Home</a></li>
-        <li className='nav-item'><a href='#about'>About</a></li>
-        <li className='nav-item'><a href='#projects'>Projects</a></li>
+        <li className='nav-item'><a href='#home' onClick={() => handlePageChange('Home')}>Home</a></li>
+        <li className='nav-item'><a href='#about' onClick={() => handlePageChange('About')}>About</a></li>
+        <li className='nav-item'><a href='#projects' onClick={() => handlePageChange('Projects')}>Projects</a></li>
       </ul>
     </div>
   );
