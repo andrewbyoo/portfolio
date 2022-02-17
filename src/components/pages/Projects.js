bd72924560077dbba15bdb11cbc60efe208a36e5
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-17 18:38:14 -0500

    Added projects from previous portfolio

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,6 +1,34 @@
 export default function Projects() {
   return (
     <div>
+      <h2>Personal Projects</h2>
+      <figure class='project-container'>
+        <article class='projects' id='project1'>
+          <h4>Tech Blog</h4>
+          <a href='https://andrewbyoo-tech-blog.herokuapp.com/' target='_blank' rel='noreferrer'><span>Deployed</span></a
+          ><a href='https://github.com/andrewbyoo/tech-blog' target='_blank' rel='noreferrer'><span>GitHub</span></a>
+        </article>
+        <article class='projects' id='project2'>
+          <h4>e-Commerce Backend</h4>
+          <a href='x-github-client://openRepo/https://github.com/andrewbyoo/e-commerce-backend' target='_blank' rel='noreferrer'><span>GitHub Desktop</span></a
+          ><a href='https://github.com/andrewbyoo/e-commerce-backend' target='_blank' rel='noreferrer'><span>GitHub</span></a>
+        </article>
+        <article class='projects' id='project3'>
+          <h4>Employee Tracker</h4>
+          <a href='x-github-client://openRepo/https://github.com/andrewbyoo/employee-tracker' target='_blank' rel='noreferrer'><span>GitHub Desktop</span></a
+          ><a href='https://github.com/andrewbyoo/employee-tracker' target='_blank' rel='noreferrer'><span>GitHub</span></a>
+        </article>
+        <article class='projects' id='project4'>
+          <h4>Team Profile Generator</h4>
+          <a href='x-github-client://openRepo/https://github.com/andrewbyoo/team-profile-generator' target='_blank' rel='noreferrer'><span>GitHub Desktop</span></a
+          ><a href='https://github.com/andrewbyoo/team-profile-generator' target='_blank' rel='noreferrer'><span>GitHub</span></a>
+        </article>
+        <article class='projects' id='project5'>
+          <h4>Notes App</h4>
+          <a href='https://andrewbyoo-notes-app.herokuapp.com/' target='_blank' rel='noreferrer'><span>Deployed</span></a
+          ><a href='https://github.com/andrewbyoo/notes-app' target='_blank' rel='noreferrer'><span>GitHub</span></a>
+        </article>
+      </figure>
     </div>
   )
 };
