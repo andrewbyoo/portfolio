ee9286fcf9ee36204ae06fc750a78bce194ab964
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-17 17:24:53 -0500

    Changed file name

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        Hi! I'm Andrew, a full-stack developer from Perkasie, Pennsylvania. I am a student at a bootcamp student at UPenn and am looking forward to applying my knowledge and learn new skills in the programming field.
+      </p>
+    </div>
+  )
+};
