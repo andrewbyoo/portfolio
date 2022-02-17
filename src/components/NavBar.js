0027b714aa600c4bb5d6a3e35b6af06583ed8156
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-17 18:37:47 -0500

    Imported css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,10 @@
+import './NavBar.css';
+
 export default function NavBar({ currentPage, handlePageChange }) {
   return (
     <div>
       <ul className='nav nav-tabs'>
-        <li className='nav-item'><h1 href='#about' onClick={() => handlePageChange('About')}>Andrew Yoo</h1></li>
+        <li className='navbar-brand'><h1 href='#about' onClick={() => handlePageChange('About')}>Andrew Yoo</h1></li>
         <li className='nav-item'><a href='#home' onClick={() => handlePageChange('Home')}>Home</a></li>
         <li className='nav-item'><a href='#about' onClick={() => handlePageChange('About')}>About</a></li>
         <li className='nav-item'><a href='#projects' onClick={() => handlePageChange('Projects')}>Projects</a></li>
