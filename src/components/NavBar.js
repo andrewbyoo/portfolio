b7b488e04b90ae6e8b17cc374af7e0dd9e95b46c
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-17 17:41:33 -0500

    Added nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-function NavBar() {
-  return
-}
-
-export default NavBar;
+export default function NavBar() {
+  return (
+    <div bg="dark" variant="dark">
+      <article>
+        <h1 href="#home">Andrew Yoo</h1>
+        <section className="me-auto">
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#projects">Projects</a>
+        </section>
+      </article>
+    </div>
+  );
+};
