653440ac93a6852710f4826c71ae91acccf10d03
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-17 17:55:02 -0500

    Added contact links

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,6 +3,13 @@ import React from 'react';
 export default function Contact() {
   return (
     <div>
+      <h2>Contact Info</h2>
+      <ul>
+        <li><a href="tel:2158476562">215.847.6562</a></li>
+        <li><a href="mailto: andrewbyoo@gmail.com">andrewbyoo@gmail.com</a></li>
+        <li id='github'><a href='https://github.com/andrewbyoo' target='_blank' rel='noreferrer'>GitHub</a></li>
+        <li id='linkedin'><a href='https://www.linkedin.com/in/andrewbyoo/' target='_blank' rel='noreferrer'>LinkedIn</a></li>
+      </ul>
     </div>
   )
 };
