670100d4cf0cf58aa8b8ec62cf08abb3eb1df483
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-17 17:32:19 -0500

    Added technical skills

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -7,6 +7,7 @@ export default function About() {
       <p>
         Hi! I'm Andrew, a full-stack developer from Perkasie, Pennsylvania. I am a student at a bootcamp student at UPenn and am looking forward to applying my knowledge and learn new skills in the programming field.
       </p>
+      <p>Technical skills: Terminal, Git, GitHub, Heroku, HTML5, CSS3, JavaScript, jQuery, Bootstrap, MiniCSS, Node.js, Npm, JSON, ECMAScript6, Unit Testing, Express, Handlebars, MySQL, Sequelize, REST APIs, MongoDB, Mongoose, Webpack, React</p>
     </div>
   )
 };
