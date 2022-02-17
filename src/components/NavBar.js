6c5d3e4480940993538dee3e96d70e679df8b8b6
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-17 18:59:56 -0500

    Added nav-link class

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,11 +5,11 @@ export default function NavBar({ currentPage, handlePageChange }) {
     <header>
       <ul className='nav nav-tabs'>
         <li className='navbar-brand'><h1 href='#about' onClick={() => handlePageChange('About')}>Andrew Yoo</h1></li>
-        <li className='nav-item'><a href='#home' onClick={() => handlePageChange('Home')}>Home</a></li>
-        <li className='nav-item'><a href='#about' onClick={() => handlePageChange('About')}>About</a></li>
-        <li className='nav-item'><a href='#projects' onClick={() => handlePageChange('Projects')}>Projects</a></li>
-        <li className='nav-item'><a href='#contact' onClick={() => handlePageChange('Contact')}>Contact</a></li>
-        <li className='nav-item'><a href='https://docs.google.com/document/d/15Qfjv31LCEDYU3t8zkX1bWsLEtJhKDihFyDq4aH4RHI/edit?usp=sharing' target='_blank' rel='noreferrer noopener'>Resume</a></li>
+        <li className='nav-item'><a href='#home' onClick={() => handlePageChange('Home')} className='nav-link'>Home</a></li>
+        <li className='nav-item'><a href='#about' onClick={() => handlePageChange('About')} className='nav-link'>About</a></li>
+        <li className='nav-item'><a href='#projects' onClick={() => handlePageChange('Projects')} className='nav-link'>Projects</a></li>
+        <li className='nav-item'><a href='#contact' onClick={() => handlePageChange('Contact')} className='nav-link'>Contact</a></li>
+        <li className='nav-item'><a href='https://docs.google.com/document/d/15Qfjv31LCEDYU3t8zkX1bWsLEtJhKDihFyDq4aH4RHI/edit?usp=sharing' target='_blank' rel='noreferrer noopener' className='nav-link'>Resume</a></li>
       </ul>
     </header>
   );
