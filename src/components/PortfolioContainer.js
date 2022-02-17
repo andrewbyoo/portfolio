5811cc875f08e42b5e62ba8f8fa21b880af2e935
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-17 17:25:39 -0500

    Imported home and about pages

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import NavBar from './NavBar';
+import Home from './pages/Home';
+import About from './pages/About';
 
 export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
   return
 }
