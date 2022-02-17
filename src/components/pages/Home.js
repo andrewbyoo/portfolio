fd669103092f736e096069fb6fc6321741ed9473
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-17 17:25:28 -0500

    Added basic self introduction

---
+++ src/components/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hi! I'm Andrew.</h1>
+      <p>Full-Stack Developer, Gamer, Husband, and Father.</p>
+    </div>
+  )
+};
