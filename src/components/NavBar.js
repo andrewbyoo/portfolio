0ab9d6860aed64e1e44e3e4e004f675b2d1cd470
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-17 18:50:03 -0500

    Added resume link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,6 +9,7 @@ export default function NavBar({ currentPage, handlePageChange }) {
         <li className='nav-item'><a href='#about' onClick={() => handlePageChange('About')}>About</a></li>
         <li className='nav-item'><a href='#projects' onClick={() => handlePageChange('Projects')}>Projects</a></li>
         <li className='nav-item'><a href='#contact' onClick={() => handlePageChange('Contact')}>Contact</a></li>
+        <li className='nav-item'><a href='https://docs.google.com/document/d/15Qfjv31LCEDYU3t8zkX1bWsLEtJhKDihFyDq4aH4RHI/edit?usp=sharing' target='_blank' rel='noreferrer noopener'>Resume</a></li>
       </ul>
     </div>
   );
