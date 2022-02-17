c273db3fd0d89488d6934e320cd7a672e3ac50e6
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-17 16:41:52 -0500

    Added strictmode back in

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 ReactDOM.render(
-    <App />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
