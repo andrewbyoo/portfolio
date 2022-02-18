fe62ca4afc1455a8e2e1489ede971caa4d82d480
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-18 18:06:20 -0500

    Added div with class homepageRender

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,13 +2,15 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 export default function Home() {
   return (
-    <Container>
-      <Row>
-        <Col className='text-center'><h1>Hi! I'm Andrew.</h1></Col>
-      </Row>
-      <Row>
-        <Col className='text-center'><p>Full-Stack Developer, Gamer, Husband, and Father.</p></Col>
-      </Row>
-    </Container>
+    <div className='homepageRender'>
+      <Container>
+        <Row>
+          <Col className='text-center'><h1>Hi! I'm Andrew.</h1></Col>
+        </Row>
+        <Row>
+          <Col className='text-center'><p>Full-Stack Developer, Gamer, Husband, and Father.</p></Col>
+        </Row>
+      </Container>
+    </div>
   )
 };
