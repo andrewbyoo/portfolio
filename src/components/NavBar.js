b1f06ef13d40199ddbd78ebd997422b01005ce10
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-18 15:05:26 -0500

    Changed href to lowercase

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,7 +13,7 @@ export default function NavBar({ currentPage, handlePageChange }) {
               <Nav.Link href='#home' onClick={() => handlePageChange('Home')} className='nav-link'>Home</Nav.Link>
               <Nav.Link href='#about' onClick={() => handlePageChange('About')} className='nav-link'>About</Nav.Link>
               <Nav.Link href='#projects' onClick={() => handlePageChange('Projects')} className='nav-link'>Projects</Nav.Link>
-              <Nav.Link href='#Contact' onClick={() => handlePageChange('Contact')} className='nav-link'>Contact</Nav.Link>
+              <Nav.Link href='#contact' onClick={() => handlePageChange('Contact')} className='nav-link'>Contact</Nav.Link>
               <Nav.Link href='https://docs.google.com/document/d/15Qfjv31LCEDYU3t8zkX1bWsLEtJhKDihFyDq4aH4RHI/edit?usp=sharing' target='_blank' rel='noreferrer noopener' className='nav-link'>Resume</Nav.Link>
             </Nav>
           </Navbar.Collapse>
