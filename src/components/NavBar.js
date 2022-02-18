1d64b74b8110522d1afe3813e893d20794cdc68c
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-18 10:39:23 -0500

    Changed navbar to use bootstrap elements

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,24 @@
+import { Container, Navbar, Nav } from 'react-bootstrap';
 import './NavBar.css';
 
 export default function NavBar({ currentPage, handlePageChange }) {
   return (
     <header>
-      <ul className='nav nav-tabs'>
-        <li className='navbar-brand'><h1 href='#about' onClick={() => handlePageChange('About')}>Andrew Yoo</h1></li>
-        <li className='nav-item'><a href='#home' onClick={() => handlePageChange('Home')} className='nav-link'>Home</a></li>
-        <li className='nav-item'><a href='#about' onClick={() => handlePageChange('About')} className='nav-link'>About</a></li>
-        <li className='nav-item'><a href='#projects' onClick={() => handlePageChange('Projects')} className='nav-link'>Projects</a></li>
-        <li className='nav-item'><a href='#contact' onClick={() => handlePageChange('Contact')} className='nav-link'>Contact</a></li>
-        <li className='nav-item'><a href='https://docs.google.com/document/d/15Qfjv31LCEDYU3t8zkX1bWsLEtJhKDihFyDq4aH4RHI/edit?usp=sharing' target='_blank' rel='noreferrer noopener' className='nav-link'>Resume</a></li>
-      </ul>
+      <Navbar collapseOnSelect sticky='top' expand='md'>
+        <Container className='nav nav-tabs'>
+          <Navbar.Brand className='navbar-brand'>Andrew Yoo</Navbar.Brand>
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse id='responsive-navbar-nav'>
+            <Nav>
+              <Nav.Link href='#home' onClick={() => handlePageChange('Home')} className='nav-link'>Home</Nav.Link>
+              <Nav.Link href='#about' onClick={() => handlePageChange('About')} className='nav-link'>About</Nav.Link>
+              <Nav.Link href='#projects' onClick={() => handlePageChange('Projects')} className='nav-link'>Projects</Nav.Link>
+              <Nav.Link href='#Contact' onClick={() => handlePageChange('Contact')} className='nav-link'>Contact</Nav.Link>
+              <Nav.Link href='https://docs.google.com/document/d/15Qfjv31LCEDYU3t8zkX1bWsLEtJhKDihFyDq4aH4RHI/edit?usp=sharing' target='_blank' rel='noreferrer noopener' className='nav-link'>Resume</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
   );
 };
