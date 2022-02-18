dc5eac85192f124a15867bcd3a84c398ace20679
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-17 19:40:18 -0500

    Added class of renderContainer

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -26,7 +26,7 @@ export default function PortfolioContainer() {
   const handlePageChange = (page) => setCurrentPage(page);
 
   return (
-    <div>
+    <div className='renderContainer'>
       <NavBar currentPage={currentPage} handlePageChange={handlePageChange} />
       {renderPage()}
     </div>
