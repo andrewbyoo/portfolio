d184354ac15a5b55e8ece77cfd7c17aebebee2ad
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-18 20:43:23 -0500

    Deconstructed props

--- src/components/ProjectTemplate.js
+++ src/components/ProjectTemplate.js
@@ -1,4 +1,4 @@
-export default function ProjectTemplate(props) {
+export default function ProjectTemplate({ title, screenshot, description, deployedLink, githubLink }) {
   return (
     <div>
       <figure className='project-container'>
@@ -6,26 +6,6 @@ export default function ProjectTemplate(props) {
           <h4>Tech Blog</h4>
           <a href='https://andrewbyoo-tech-blog.herokuapp.com/' target='_blank' rel='noreferrer'><span>Deployed</span></a
           ><a href='https://github.com/andrewbyoo/tech-blog' target='_blank' rel='noreferrer'><span>GitHub</span></a>
-        </article>
-        <article className='projects' id='project2'>
-          <h4>e-Commerce Backend</h4>
-          <a href='x-github-client://openRepo/https://github.com/andrewbyoo/e-commerce-backend' target='_blank' rel='noreferrer'><span>GitHub Desktop</span></a
-          ><a href='https://github.com/andrewbyoo/e-commerce-backend' target='_blank' rel='noreferrer'><span>GitHub</span></a>
-        </article>
-        <article className='projects' id='project3'>
-          <h4>Employee Tracker</h4>
-          <a href='x-github-client://openRepo/https://github.com/andrewbyoo/employee-tracker' target='_blank' rel='noreferrer'><span>GitHub Desktop</span></a
-          ><a href='https://github.com/andrewbyoo/employee-tracker' target='_blank' rel='noreferrer'><span>GitHub</span></a>
-        </article>
-        <article className='projects' id='project4'>
-          <h4>Team Profile Generator</h4>
-          <a href='x-github-client://openRepo/https://github.com/andrewbyoo/team-profile-generator' target='_blank' rel='noreferrer'><span>GitHub Desktop</span></a
-          ><a href='https://github.com/andrewbyoo/team-profile-generator' target='_blank' rel='noreferrer'><span>GitHub</span></a>
-        </article>
-        <article className='projects' id='project5'>
-          <h4>Notes App</h4>
-          <a href='https://andrewbyoo-notes-app.herokuapp.com/' target='_blank' rel='noreferrer'><span>Deployed</span></a
-          ><a href='https://github.com/andrewbyoo/notes-app' target='_blank' rel='noreferrer'><span>GitHub</span></a>
         </article>
       </figure>
     </div>
