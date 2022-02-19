b7355e641899805177fac1918453c529046cb17a
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-18 20:33:31 -0500

    Added previously listed projects into projectList array

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -7,8 +7,42 @@ const projectList = [
     description: 'Project Description 1',
     deployedLink: 'Deployed Link 1',
     githubLink: 'Github Link 1',
-
-  }
+  },
+  {
+    title: 'Tech Blog',
+    screenshot: 'Project Screenshot 2',
+    description: 'Project Description 2',
+    deployedLink: 'https://andrewbyoo-tech-blog.herokuapp.com/',
+    githubLink: 'https://github.com/andrewbyoo/tech-blog',
+  },
+  {
+    title: 'e-Commerce Backend',
+    screenshot: 'Project Screenshot 3',
+    description: 'Project Description 3',
+    deployedLink: null,
+    githubLink: 'https://github.com/andrewbyoo/e-commerce-backend',
+  },
+  {
+    title: 'Employee Tracker',
+    screenshot: 'Project Screenshot 4',
+    description: 'Project Description 4',
+    deployedLink: null,
+    githubLink: 'https://github.com/andrewbyoo/employee-tracker',
+  },
+  {
+    title: 'Team Profile Generator',
+    screenshot: 'Project Screenshot 5',
+    description: 'Project Description 5',
+    deployedLink: null,
+    githubLink: 'https://github.com/andrewbyoo/team-profile-generator',
+  },
+  {
+    title: 'Notes App',
+    screenshot: 'Project Screenshot 6',
+    description: 'Project Description 6',
+    deployedLink: 'https://andrewbyoo-notes-app.herokuapp.com/',
+    githubLink: 'https://github.com/andrewbyoo/notes-app',
+  },
 ]
 
 const projectsRender = projectList.map((project) => <ProjectTemplate projectInfo={project}/>)
