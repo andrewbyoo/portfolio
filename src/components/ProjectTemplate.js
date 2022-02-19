75fd387f6f4316eb2316397e970d905477cfd651
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-18 23:39:44 -0500

    Added bootstrap classes to card body

--- src/components/ProjectTemplate.js
+++ src/components/ProjectTemplate.js
@@ -5,7 +5,7 @@ export default function ProjectTemplate({ title, screenshot, altDesc, descriptio
     <div className='individualProject'>
       <Card style={{ width: '20rem' }}>
         <Card.Img variant="top" className='projectImages' src={screenshot} alt={altDesc} />
-        <Card.Body>
+        <Card.Body className='d-flex flex-column justify-content-between'>
           <Card.Title>{title}</Card.Title>
           <Card.Text>{description}</Card.Text>
           <Button href={deployedLink} target='_blank' rel='noreferrer noopener' className='projectButton'>Deployed</Button>
