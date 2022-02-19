c567411f0adb3617800f407f46beccaa81fa50a4
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-18 21:27:20 -0500

    Added class for the container div and changed classes on buttons to be the same

--- src/components/ProjectTemplate.js
+++ src/components/ProjectTemplate.js
@@ -2,14 +2,14 @@ import { Card, Button } from 'react-bootstrap';
 
 export default function ProjectTemplate({ title, screenshot, description, deployedLink, githubLink }) {
   return (
-    <div>
-      <Card style={{ width: '18rem' }}>
+    <div className='individualProject'>
+      <Card style={{ width: '20rem' }}>
         <Card.Img variant="top" src="holder.js/100px180" />
         <Card.Body>
           <Card.Title>{title}</Card.Title>
           <Card.Text>{description}</Card.Text>
-          <Button href={deployedLink} target='_blank' rel='noreferrer noopener' className='deployedButton'>Deployed</Button>
-          <Button href={githubLink} target='_blank' rel='noreferrer noopener' className='githubButton'>Github</Button>
+          <Button href={deployedLink} target='_blank' rel='noreferrer noopener' className='projectButton'>Deployed</Button>
+          <Button href={githubLink} target='_blank' rel='noreferrer noopener' className='projectButton'>Github</Button>
         </Card.Body>
       </Card>
     </div>
