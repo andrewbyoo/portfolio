72433a6d7f691486e4c7e85d37e0db8f2abe0f15
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-19 13:28:07 -0500

    Changed description

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -9,7 +9,7 @@ export default function About() {
           <Col sm='12' lg='8'>
             <h1>About Me</h1>
             <p>
-              Hi! I'm Andrew, a full-stack developer from Perkasie, Pennsylvania with a background in accounting and commercial loan banking. I am known by colleagues to be a fast learner and have strong critical thinking and analytical skills. I am a bootcamp student  at UPenn and am looking forward to applying my knowledge and learn new skills in the programming field.
+              Hi! I'm Andrew, a full-stack developer from Perkasie, Pennsylvania with a background in accounting and commercial loan banking. I will be earning a certificate in full stack development come March 2nd from the University of Pennsylvania with skills in CSS, JavaScript, and React.js. Known by colleagues to be a fast learner and have strong critical thinking and analytical skills. I am looking forward to apply my knowledge and learn new skills in the programming field.
             </p>
             <p>Technical skills: Terminal, Git, GitHub, Heroku, HTML5, CSS3, JavaScript, jQuery, Bootstrap, MiniCSS, Node.js, Npm, JSON, ECMAScript6, Unit Testing, Express, Handlebars, MySQL, Sequelize, REST APIs, MongoDB, Mongoose, Webpack, React</p>
           </Col>
