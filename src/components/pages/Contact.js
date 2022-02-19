3544bcc932ab6b23cbf14b79b222f1c42ff081d1
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-19 03:00:04 -0500

    Set form submit function to alert that form is a work in progress

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -21,6 +21,17 @@ function Form(){
       setComments(inputValue);
     }
   }
+
+  const handleSubmitForm = (event) => {
+    event.preventDefault();
+
+    alert('Contact form is a work in progress. Please email me directly by click on my email link and I will get to you as soon as possible.')
+
+    setName('');
+    setNumber('');
+    setEmail('');
+    setComments('');
+  }
 }
 
 export default function Contact() {
